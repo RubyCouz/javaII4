408d2e055e7e6ff84f6dba57c332adb0f37b5b2b
rubycouzcedric <cousin.ruby.cedric@gmail.com>
2018-06-22 16:07:35 +0200

    partie2 exo4

---
+++ assets/js/java.js
@@ -0,0 +1,6 @@
+function resetFields () {
+  document.getElementById('lastname').value = '';
+  document.getElementById('firstname').value = '';
+  document.getElementById('city').value = '';
+  document.getElementById('select1').selectedIndex = -1;
+}
