f51869885d0dbbf4e5d90031485ce281fa6f4742
rubycouzcedric <cousin.ruby.cedric@gmail.com>
2018-06-25 13:49:34 +0200

    correction

--- assets/js/java.js
+++ assets/js/java.js
@@ -1,6 +1,7 @@
+//déclaration de la fonction qui permet de vider les champs du formulaire.
 function resetFields () {
   document.getElementById('lastname').value = '';
   document.getElementById('firstname').value = '';
   document.getElementById('city').value = '';
-  document.getElementById('select1').selectedIndex = -1;
 }
+// ''=chaine vide
